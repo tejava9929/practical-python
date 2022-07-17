ac6f673c028c3d96a791f0aff3c28d7d6e22c410
tejava9929 <kimty0044@naver.com>
2022-07-17 17:16:41 +0900

    1.3 완료

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+InitialHeight = 100
+
+Height = InitialHeight
+for i in range(10):
+    Height = Height*3/5
+    print(i+1, end=' ')
+    print(round(Height,4))
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+#원금
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+total_paid = 0.0
+month = 0
+while principal>0 :
+    principal = principal*(1+rate/12)-payment
+    total_paid += payment
+    if month>=extra_payment_start_month and month<=extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    if principal<0 :
+        total_paid += principal
+        principal = 0
+    month += 1
+    print(month, total_paid, principal)
+    #python에서 print에서는, 변수 사이에 공백 자동 삽입
+    
+print('Total paid', total_paid)
+print('Months', month)
+
